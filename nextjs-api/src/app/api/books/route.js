f3b8f96486cfaa18e33d1575159bbff8d383b46e
SuMyatNoeOo534 <sumyatnoeoo750@gmail.com>
2025-06-23 10:38:28 +0630

    update

--- nextjs-api/src/app/api/books/route.js
+++ nextjs-api/src/app/api/books/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import * as yup from 'yup';
 const BookData=[
     {
         id:1,
@@ -26,13 +27,63 @@ const BookData=[
 export async function GET(){
     return NextResponse.json(BookData);
 }
-export async function POST(req){
-    //const body=await req.json();
+const schema=yup.object().shape({
+    title:yup.string().required("Title is required"),
+    author:yup.string().required("Author is required"),
+    published_year:yup.number().required("Published_year is required"),
     
+});
+export async function POST(req){
+    try{
+        const body=await req.json();
+    await schema.validate(body,{abortEarly:false});
     return NextResponse.json({
-        message:"Book is successfully created",
-        // bodyData:body,
+        message:"Book is successfullycreated",
+        bodyData:body,
     });
     
+    
+    
+}catch(error){
+//   return NextResponse.json({message:"Internal server error"},
+//     {
+//         status:500,
+//     }
+// );
+if(error.name==="ValidationError"){
+    return NextResponse.json(
+        {
+            message:"Validation failed",
+            errors:error.inner.map((e)=>({
+                path:e.path,
+                message:e.message,
+            })),
+
+            },
+            {status:400}
+    );
+    
 }
+return NextResponse.json(
+    {
+        message:"Unexpected error",
+        error:error.message,
+    },
+    {status:500}
+);
+}
+}   
+
+// export async function GET(){
+//     return NextResponse.json(BookData);
+// }
+// export async function POST(req){
+//     //const body=await req.json();
+    
+//     return NextResponse.json({
+//         message:"Book is successfully created",
+//         // bodyData:body,
+//     });
+    
+// }
     
--- nextjs-api/src/app/api/students/route.js
+++ nextjs-api/src/app/api/students/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import * as yup from 'yup';
 const StudentData=[
     {
         id:1,
@@ -25,13 +26,54 @@ const StudentData=[
 export async function GET(){
     return NextResponse.json(StudentData);
 }
-export async function POST(req){
-    const body=await req.json();
+const schema=yup.object().shape({
+    name:yup.string().required("Name is required"),
+    fatherName:yup.string().required("FatherName is required"),
+    address:yup.string().required("Address is required"),
+    age:yup.number().required("Age is required"),
     
+    major:yup.string().required("Major is required"),
+
+
+
+})
+export async function POST(req){
+    try{
+        const body=await req.json();
+    await schema.validate(body,{abortEarly:false});
     return NextResponse.json({
         message:"Student is successfullycreated",
         bodyData:body,
     });
-    bodyData:body;
-}
     
+    
+    
+}catch(error){
+//   return NextResponse.json({message:"Internal server error"},
+//     {
+//         status:500,
+//     }
+// );
+if(error.name==="ValidationError"){
+    return NextResponse.json(
+        {
+            message:"Validation failed",
+            errors:error.inner.map((e)=>({
+                path:e.path,
+                message:e.message,
+            })),
+
+            },
+            {status:400}
+    );
+    
+}
+return NextResponse.json(
+    {
+        message:"Unexpected error",
+        error:error.message,
+    },
+    {status:500}
+);
+}
+}   
